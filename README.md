4c88cb31ac9066eb07e2310cd2b18c323d659e6f
Gregory P. Akins <gregory.akins@semanticbits.com>
2022-03-03 20:46:51 -0500

    MAT-3901: Initial project creation

---
+++ README.md
@@ -0,0 +1,1 @@
+# cql-library-service
---
+++ src/main/java/gov/cms/madie/cqllibraryservice/CqlLibraryServiceApplication.java
@@ -0,0 +1,12 @@
+package gov.cms.madie.cqllibraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqlLibraryServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CqlLibraryServiceApplication.class, args);
+  }
+}
