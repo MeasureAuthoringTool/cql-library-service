bee3a050c12a12467f65f3a25240854a692d9136
Nate Moraca <nate.moraca@semanticbits.com>
2022-03-16 12:10:55 -0400

    added model and validations; ref #MAT-3903

---
+++ src/main/java/gov/cms/madie/cqllibraryservice/validators/EnumValidator.java
@@ -0,0 +1,24 @@
+package gov.cms.madie.cqllibraryservice.validators;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotNull;
+import java.lang.annotation.*;
+
+@NotNull(message = "Value cannot be null")
+@ReportAsSingleViolation
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = EnumValidatorImpl.class)
+public @interface EnumValidator {
+
+  Class<? extends Enum<?>> enumClass();
+
+  String message() default "Value provided is not a valid option.";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
