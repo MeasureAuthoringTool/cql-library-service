3a5c715ec2765a1ffe02af031872c2f3d7158b83
Nate Moraca <nate.moraca@semanticbits.com>
2022-04-18 11:38:40 -0400

    make name check case-insensitive; ref #MAT-4197

--- src/main/java/gov/cms/madie/cqllibraryservice/repositories/CqlLibraryRepository.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/repositories/CqlLibraryRepository.java
@@ -1,15 +1,19 @@
 package gov.cms.madie.cqllibraryservice.repositories;
 
 import gov.cms.madie.cqllibraryservice.models.CqlLibrary;
+import org.springframework.data.mongodb.repository.ExistsQuery;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CqlLibraryRepository
     extends MongoRepository<CqlLibrary, String>, CqlLibraryVersionRepository {
+
   Optional<CqlLibrary> findByCqlLibraryName(String cqlLibraryName);
 
+  @ExistsQuery("{cqlLibraryName: {$regex: '^?0$', $options: 'i'}}")
   boolean existsByCqlLibraryName(String cqlLibraryName);
 
   boolean existsByGroupIdAndDraft(String groupId, boolean draft);
