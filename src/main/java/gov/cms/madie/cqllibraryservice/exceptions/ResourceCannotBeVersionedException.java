63f113c750dfe770c805e9413e7c655fab5582a5
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-04-18 15:52:38 -0400

    Resolved checkstyle issue

--- src/main/java/gov/cms/madie/cqllibraryservice/exceptions/ResourceCannotBeVersionedException.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/exceptions/ResourceCannotBeVersionedException.java
@@ -1,9 +1,10 @@
 package gov.cms.madie.cqllibraryservice.exceptions;
 
 public class ResourceCannotBeVersionedException extends RuntimeException {
-    private static final String MESSAGE = "User %s cannot version resource %s with id: %s as the Cql has errors in it";
+  private static final String MESSAGE =
+      "User %s cannot version resource %s with id: %s as the Cql has errors in it";
 
-    public ResourceCannotBeVersionedException(String type, String id, String user) {
-        super(String.format(MESSAGE, user, type, id));
-    }
+  public ResourceCannotBeVersionedException(String type, String id, String user) {
+    super(String.format(MESSAGE, user, type, id));
+  }
 }
