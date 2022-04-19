c5b54d2bde64c69b95660c61ca56e78634a9abb4
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-04-19 08:14:39 -0400

    merged develop

--- src/main/java/gov/cms/madie/cqllibraryservice/services/VersionService.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/services/VersionService.java
@@ -49,7 +49,9 @@ public class VersionService {
     if (cqlLibrary.isCqlErrors()) {
       log.error(
           "User [{}] cannot create a version for CQL Library with id [{}] "
-              + "as the Cql has errors in it", username, cqlLibrary.getId());
+              + "as the Cql has errors in it",
+          username,
+          cqlLibrary.getId());
       throw new ResourceCannotBeVersionedException("CQL Library", cqlLibrary.getId(), username);
     }
 
