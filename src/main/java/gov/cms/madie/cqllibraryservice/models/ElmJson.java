7d9729fff838bf06481e0b69b67bf84a113971a7
Nate Moraca <nate.moraca@semanticbits.com>
2022-05-04 16:06:44 -0400

    fix for jackson mapping requiring constructor; ref #MAT-4192

--- src/main/java/gov/cms/madie/cqllibraryservice/models/ElmJson.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/models/ElmJson.java
@@ -1,10 +1,14 @@
 package gov.cms.madie.cqllibraryservice.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ElmJson {
   private String json;
   private String xml;
