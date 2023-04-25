1aa880fefc001e058a4eded69690c242dbf27daf
Nate Moraca <nate.moraca@semanticbits.com>
2023-04-25 08:13:24 -0400

    remove new API key filter as it is no longer needed

--- src/main/java/gov/cms/madie/cqllibraryservice/config/security/SecurityConfig.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/config/security/SecurityConfig.java
@@ -1,19 +1,14 @@
 package gov.cms.madie.cqllibraryservice.config.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
-
-  @Value("${read-api-key}")
-  private String madieReadApiKey;
 
   @Bean
   protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -30,10 +25,6 @@ public class SecurityConfig {
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
-        .addFilterBefore(
-            new ApiTokenSecurityFilter(
-                madieReadApiKey, "/cql-libraries/versioned", "/cql-libraries/cql"),
-            BearerTokenAuthenticationFilter.class)
         .oauth2ResourceServer()
         .jwt()
         .and()
