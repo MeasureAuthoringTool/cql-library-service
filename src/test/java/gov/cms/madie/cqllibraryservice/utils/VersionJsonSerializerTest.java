02edc2ed777dde1824ecf740b21cde1678e04e3f
CeciliaLiu8 <cecilialiu8@gmail.com>
2023-06-14 13:30:24 -0500

    disable mongock for tests

--- src/test/java/gov/cms/madie/cqllibraryservice/utils/VersionJsonSerializerTest.java
+++ src/test/java/gov/cms/madie/cqllibraryservice/utils/VersionJsonSerializerTest.java
@@ -13,9 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @Slf4j
 @SpringBootTest
+@TestPropertySource(properties = {"mongock.enabled=false"})
 class VersionJsonSerializerTest {
 
   @Autowired private ObjectMapper objectMapper;
