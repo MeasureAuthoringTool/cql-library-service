e566593ea98186623595275f2b1879b484976f7b
adongare <ashok.dongare@semanticbits.com>
2023-06-15 08:34:42 -0400

    fix failing tests

--- src/test/java/gov/cms/madie/cqllibraryservice/utils/VersionJsonSerializerTest.java
+++ src/test/java/gov/cms/madie/cqllibraryservice/utils/VersionJsonSerializerTest.java
@@ -13,8 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @Slf4j
+@ActiveProfiles("test")
 @SpringBootTest
 class VersionJsonSerializerTest {
 
