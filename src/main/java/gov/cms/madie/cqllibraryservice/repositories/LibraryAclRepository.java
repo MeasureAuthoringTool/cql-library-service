10c92af3618cadcf9c36673b73b2aeb52b034ca8
mcmcphillips <matt.mcphillips@semanticbits.com>
2023-06-23 07:22:17 -0700

    Update documentation

--- src/main/java/gov/cms/madie/cqllibraryservice/repositories/LibraryAclRepository.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/repositories/LibraryAclRepository.java
@@ -7,16 +7,28 @@ import java.util.List;
 
 public interface LibraryAclRepository {
   /**
-   * Library is considered to be my measure if provided user is the owner of this measure or is
-   *
+   * Library is considered to be my library if the libraryset.owner is the userid provided
    * @param userId- current user
-   * @return Pageable List of measures
+   * @return List of cqlLibraries
    */
   List<CqlLibrary> findAllMyLibraries(String userId);
-
+  /**
+   * @param cqlLibraryName- current user
+   * @param draft- current user
+   * @param cqlLibraryName- current user
+   * @param version- current user
+   * @return List of cqlLibraries
+   */
   List<CqlLibrary> findAllByCqlLibraryNameAndDraftAndVersion(
       String cqlLibraryName, boolean draft, Version version);
-
+  /**
+   * @param cqlLibraryName- current user
+   * @param draft- current user
+   * @param cqlLibraryName- current user
+   * @param version- current user
+   * @param model- current user
+   * @return List of cqlLibraries
+   */
   List<CqlLibrary> findAllByCqlLibraryNameAndDraftAndVersionAndModel(
       String cqlLibraryName, boolean draft, Version version, String model);
 }
