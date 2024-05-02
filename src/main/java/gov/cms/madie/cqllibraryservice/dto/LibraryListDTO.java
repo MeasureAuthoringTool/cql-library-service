645a2e650f4ef077a72c86845abcafd84e7440fe
Christopher Hubert <christopher.hubert@semanticbits.com>
2024-05-02 10:21:56 -0400

    MAT-7148: remove unused import

--- src/main/java/gov/cms/madie/cqllibraryservice/dto/LibraryListDTO.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/dto/LibraryListDTO.java
@@ -12,7 +12,6 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
