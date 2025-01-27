eaf808263a61b1e2888f441d4b7ae4a919de5cf1
Edwin Tan <N119494@icf.com>
2025-01-27 14:46:11 -0800

    MAT-8084: fix error found by madie-checkstyle.xml ruleset

--- src/main/java/gov/cms/madie/cqllibraryservice/exceptions/HarpIdMismatchException.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/exceptions/HarpIdMismatchException.java
@@ -4,7 +4,8 @@ public class HarpIdMismatchException extends RuntimeException {
 
   private static final String MESSAGE =
       "Response could not be completed because the HARP id of %s passed in does not " +
-          "match the owner of the library with the library id of %s. The owner of the library is %s";
+          "match the owner of the library with the library id of %s. The owner of " +
+          "the library is %s";
 
   public HarpIdMismatchException(String harpId, String owner, String libraryId) {
     super(String.format(MESSAGE, harpId, libraryId, owner));
