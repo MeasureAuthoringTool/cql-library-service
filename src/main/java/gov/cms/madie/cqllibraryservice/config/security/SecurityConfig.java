09c520b2309c4bc98d3c81ae1f5b7dcefa63143f
nmorasb <nate.moraca@semanticbits.com>
2026-07-08 10:13:48 -0400

    MAT-10223: add new API key filter to allow a subset of endpoints to be called without requiring Okta JWT

--- src/main/java/gov/cms/madie/cqllibraryservice/config/security/SecurityConfig.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package gov.cms.madie.cqllibraryservice.config.security;
 
+import gov.cms.madie.cqllibraryservice.utils.ApiKeyFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -8,11 +10,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
   private static final String[] AUTH_WHITELIST = {"/actuator/**"};
+
+  private final String apiKeyHeader;
+  private final String apiKeyValue;
+
+  public SecurityConfig(
+      @Value("${madie.security.api-key-header}") String apiKeyHeader,
+      @Value("${madie.security.system-api-key}") String apiKeyValue) {
+    this.apiKeyHeader = apiKeyHeader;
+    this.apiKeyValue = apiKeyValue;
+  }
 
   @Bean
   protected SecurityFilterChain filterChain(HttpSecurity http, UserRoleConverter roleConverter)
@@ -38,6 +52,9 @@ public class SecurityConfig {
             oAuth2ResourceServerConfigurer ->
                 oAuth2ResourceServerConfigurer.jwt(
                     jwt -> jwt.jwtAuthenticationConverter(roleConverter)))
+        // It must run after OAuth2 processing handles the Okta token
+        .addFilterAfter(new ApiKeyFilter(apiKeyHeader, apiKeyValue, List.of("/cql-libraries/cql")),
+            org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter.class)
         .headers()
         .xssProtection()
         .and()
