ac6e51f1aaf1cc4397fc6d39f9a7746a945ad93c
adongare <ashok.dongare@semanticbits.com>
2026-07-17 10:19:37 -0400

    MAT-10216 removed unused exception

--- src/main/java/gov/cms/madie/cqllibraryservice/exceptions/VirusDetectedException.java
+++
@@ -1,9 +0,0 @@
-package gov.cms.madie.cqllibraryservice.exceptions;
-
-/** Thrown when a virus scan identifies an infected file in a downloaded FHIR package. */
-public class VirusDetectedException extends RuntimeException {
-
-  public VirusDetectedException(String message) {
-    super(message);
-  }
-}
