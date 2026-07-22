e16a60eb7154cc8e29a5050ed1d8b38a55f027fe
adongare <ashok.dongare@semanticbits.com>
2026-07-22 11:20:11 -0400

    MAT-10215 address PR review

--- src/main/java/gov/cms/madie/cqllibraryservice/config/AsyncConfig.java
+++
@@ -1,8 +0,0 @@
-package gov.cms.madie.cqllibraryservice.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@Configuration
-@EnableAsync
-public class AsyncConfig {}
