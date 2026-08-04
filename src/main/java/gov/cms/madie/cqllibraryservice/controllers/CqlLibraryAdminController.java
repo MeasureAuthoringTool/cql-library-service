b1e1ca3c23451c7f614bda67b88061e936b80d0b
adongare <ashok.dongare@semanticbits.com>
2026-08-04 13:51:28 -0400

    MAT-10217 update docstring

--- src/main/java/gov/cms/madie/cqllibraryservice/controllers/CqlLibraryAdminController.java
+++ src/main/java/gov/cms/madie/cqllibraryservice/controllers/CqlLibraryAdminController.java
@@ -140,8 +140,9 @@ public class CqlLibraryAdminController {
 
   /**
    * Asynchronous operation that initiates the installation of an IG (Implementation Guide) package
-   * for a given package ID and version. It downloads the IG(including its transitive dependencies)
-   * and then imports the CQL Libraries from it(including its transitive dependencies).
+   * for a given package ID (e.g. {@code hl7.fhir.us.qicore}) and version (e.g. {@code 7.0.2}). It
+   * downloads the IG(including its transitive dependencies) and then imports the CQL Libraries from
+   * it(including its transitive dependencies).
    *
    * @param principal the currently authenticated user
    * @param request the IG package installation request containing the {@code packageId} and {@code
